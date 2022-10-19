2db90520770572f9c1dff7f0ce76216fc42931a5
Fatma <fmohamed34@gmail.com>
2022-10-19 11:53:17 -0400

    A few exercises added.

--- ES6Scripts/es_cheap_candy.js
+++ ES6Scripts/es_cheap_candy.js
@@ -7,4 +7,13 @@ let products = [
     //
     ];
    
+   // a. which candies cost less than $4.00?
+    products.filter(p => p.price < 4).forEach(p => console.log("<4: ", p));
 
+    // b. which candies have "M&M" as part of the name?
+    let mm = products.filter(p => p.product.includes("M&M"));
+    mm.forEach(p => console.log("M&Ms: ", p));
+
+    // c. Do we have a product called Swedish Fish? 
+    //     (use find (faster) or filter (more normal) ) true==yes
+    console.log(products.find(p => p.product == "Swedish Fish") != undefined)
